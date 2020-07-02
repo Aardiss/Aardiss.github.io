3f3fe2cd2e8df679b418243da888d2d71f959ec6
Anastasia <aardiss@mail.ru>
2020-07-02 17:23:58 +0300

    слайдер выполнен

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+const slider = $('.slider').bxSlider({
+  pager: false,
+  controls: false
+});
+
+$(".section-bar__arrow-back").click(e => {
+  e.preventDefault();
+  slider.goToPrevSlide();
+
+})
+$(".section-bar__arrow-forward").click(e => {
+  e.preventDefault();
+  slider.goToNextSlide();
+})
