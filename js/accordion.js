2287c6acf2e6e15f02dfe02e7d21cc7d7c06d601
Aardiss <aardiss@mail.ru>
2020-07-03 17:25:08 +0300

    правка аккордеона

--- js/accordion.js
+++ js/accordion.js
@@ -1,22 +1,29 @@
-const acc = document.getElementsByClassName("command__profile-name");
-const container = document.getElementsByClassName("command__profile");
-
-// const closeEvery = container => {
-//    const items = container.find('command__profile-panel');
-//    items.height(0);
-// }
-
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    // const container =$this.closest("command__list");
-    //   closeEvery(container);
-    const panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    }else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-    });
+const closeOthers = list => {
+  const panel = list.find(".command__profile-panel");
+  const name = list.find(".command__profile-name");
+ 
+  name.removeClass("active");
+  panel.height(0);
 }
-    
+
+const openNew = block => {
+ const profile = block.closest(".command__profile");
+ const panel = profile.find(".command__profile-panel");
+ const panelBlock = profile.find(".command__profile-block");
+ const scrollHeight = panelBlock.height();
+
+ block.addClass("active");
+ panel.height(scrollHeight);
+}
+
+$(".command__profile-name").click((e) => {   
+   const $this = $(e.currentTarget);
+   const list = $this.closest(".command__list"); 
+
+   if ($this.hasClass("active")) {
+    closeOthers(list);
+   } else {
+    closeOthers(list);
+    openNew($this);
+   }
+  });
