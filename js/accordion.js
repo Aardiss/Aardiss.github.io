13c990de82a6dea5b6e085f42a65063934c4c8de
Anastasia <aardiss@mail.ru>
2020-07-03 12:27:04 +0300

    работа с формойй

--- js/accordion.js
+++ js/accordion.js
@@ -1,16 +1,16 @@
 const acc = document.getElementsByClassName("command__profile-name");
 const container = document.getElementsByClassName("command__profile");
 
-const closeEvery = container => {
-   const items = container.find('command__profile-panel');
-   items.height(0);
-}
+// const closeEvery = container => {
+//    const items = container.find('command__profile-panel');
+//    items.height(0);
+// }
 
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    const container =$this.closest("command__list");
-      closeEvery(container);
+    // const container =$this.closest("command__list");
+    //   closeEvery(container);
     const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
