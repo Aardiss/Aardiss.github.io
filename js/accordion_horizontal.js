93da599d79a730817dd24f788326d5e357359d03
Aardiss <aardiss@mail.ru>
2020-07-09 16:20:09 +0300

    аккордеон меню

--- js/accordion_horizontal.js
+++ js/accordion_horizontal.js
@@ -2,11 +2,11 @@
 const mesureWidth = item => {
   let reqItemWidth = 0;
   const screenWidth = $(window).width();
-  const container = item.closest("assort__list");
-  const titlesBlocks = container.find("assort__trigger");
+  const container = item.closest(".assort__list");
+  const titlesBlocks = container.find(".assort__trigger");
   const titlesWidth = titlesBlocks.width() * titlesBlocks.length;
 
-  const textContainer = item .find("assort__content");
+  const textContainer = item .find(".assort__content");
   const paddingLeft = parseInt(textContainer.css("padding-left"));
   const paddingRight = parseInt(textContainer.css("padding-right"));
 
@@ -25,30 +25,30 @@ const mesureWidth = item => {
 };
 
 const closeEveryItemContainer = container => {
-  const item = container.find("assort__item");
-  const content = container.find("assort__wrap");
+  const item = container.find(".assort__item");
+  const content = container.find(".assort__wrap");
 
-  items.removeClass("assort-active");
+  item.removeClass("assort-active");
   content.width(0);
 }
 
 const openItem = (item) => {
-  const hiddenContent = item.find("assort__wrap");
+  const hiddenContent = item.find(".assort__wrap");
   const reqWidth = mesureWidth(item);
-  const textBlock = item.find("assort__content");
+  const textBlock = item.find(".assort__content");
 
   item.addClass("assort-active");
   hiddenContent.width(reqWidth.container);
   textBlock.width(reqWidth.textContainer);
 }
 
-$("assort__trigger").on ("click", e => {
+$(".assort__trigger").on ("click", e => {
   e.preventDefault();
 
   const $this = $(e.currentTarget);
-  const item = $this.closest("assort__item");
+  const item = $this.closest(".assort__item");
   const itemOpened = item.hasClass("assort-active");
-  const container = $this.closest("assort__list");
+  const container = $this.closest(".assort__list");
 
   if (itemOpened) {
     closeEveryItemContainer(container)
@@ -58,8 +58,8 @@ $("assort__trigger").on ("click", e => {
   }
 });
 
-$("assort__close").on("click", e => {
+$(".assort__close").on("click", e => {
   e.preventDefault();
 
-  closeEveryItemContainer($('assort__list'));
+  closeEveryItemContainer($('.assort__list'));
 })
