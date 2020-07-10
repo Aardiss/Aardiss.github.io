82443881e6763e56898c42afd93a8c7acc068450
Anastasia <aardiss@mail.ru>
2020-07-10 19:35:59 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
-const { src, dest } = require("gulp");
+const { src, dest, task, series } = require("gulp");
+const rm = require('gulp-rm');
+const sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+task('clean', () => {
+ return src('dist/**/*', { read: false })
+   .pipe(rm())
+});
+ 
+task('copy', () => {
+ return src('./scss/components/*.scss').pipe(dest('dist'));
+});
+ 
+task('styles', () => {
+ return src('./scss/main.scss')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(dest('dist'));
+});
 
-function copy() {
-  return src('src/styles/main.scss').pipe(dest('dist'))
-}
+ 
+task('default', series('clean', 'styles'));
 
-exports.copy = copy
